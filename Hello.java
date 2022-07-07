4920d3dde5f01b97aae9c48fce7b7ae4267c5ac6
WangYongsheng <1925818135@qq.com>
2022-07-07 09:34:22 +0800

    hot-fix first commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 Class Hello {
 	public static void main(String[] args) {
-		System.out.println("hello world");//打印
+		System.out.println("hello world");//打印hello world
 		int a = 5;
 		int b = 3;
 		int res = max(a, b);
